c711af5ef5f4f408b144d336d5ee471d1268c236
kristoferlund <kristofer@fmckl.se>
2023-07-20 12:06:00 +0200

    First commit

---
+++ netlify/functions/email.ts
@@ -0,0 +1,54 @@
+import type { Handler, HandlerContext, HandlerEvent } from "@netlify/functions";
+
+import { CourierClient } from "@trycourier/courier";
+
+type requestBody = {
+  email: string;
+  amount: string;
+  payer: string;
+};
+
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: "Method Not Allowed",
+    };
+  }
+
+  if (!event.body) {
+    return {
+      statusCode: 400,
+      body: "Bad Request",
+    };
+  }
+
+  const body: requestBody = JSON.parse(event.body);
+
+  const courier = CourierClient({
+    authorizationToken: process.env.COURIER_AUTH_TOKEN,
+  });
+
+  const { requestId } = await courier.send({
+    message: {
+      to: {
+        email: body.email,
+      },
+      template: "WJKFSV1362MGZEHW9G7EMMPZDMMW",
+      data: {
+        amount: body.amount,
+        payer: body.payer,
+      },
+    },
+  });
+
+  return {
+    statusCode: 200,
+    body: requestId,
+  };
+};
+
+export { handler };
