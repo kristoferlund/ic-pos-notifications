f0c4b1a6d4736ee5d99021a5acd424abc6de391c
kristoferlund <kristofer@fmckl.se>
2023-07-20 22:42:54 +0200

    Refactor and document

--- netlify/functions/notify.ts
+++ netlify/functions/notify.ts
@@ -2,6 +2,7 @@ import type { Handler, HandlerContext, HandlerEvent } from "@netlify/functions";
 
 import { CourierClient } from "@trycourier/courier";
 
+// Define the request body type
 type requestBody = {
   idempotencyKey: string;
   email: string;
@@ -10,12 +11,15 @@ type requestBody = {
   payer: string;
 };
 
+// Define the handler function
 const handler: Handler = async (
   event: HandlerEvent,
   context: HandlerContext
 ) => {
+  // Log the event details
   console.log(`${event.httpMethod} - ${event.path} - ${event.body}`);
 
+  // Check if the HTTP method is POST
   if (event.httpMethod !== "POST") {
     return {
       statusCode: 405,
@@ -23,6 +27,7 @@ const handler: Handler = async (
     };
   }
 
+  // Check if the event body exists
   if (!event.body) {
     return {
       statusCode: 400,
@@ -30,17 +35,22 @@ const handler: Handler = async (
     };
   }
 
+  // Parse the event body
   const body: requestBody = JSON.parse(event.body);
 
+  // Convert the amount to a number and adjust its scale
   let amount = Number.parseInt(body.amount);
   amount = amount * Math.pow(10, -9);
 
+  // Initialize the Courier client
   const courier = CourierClient({
     authorizationToken: process.env.COURIER_AUTH_TOKEN,
   });
 
+  // Initialize an array to store the responses
   const responses = [];
 
+  // If an email is provided, send an email
   if (body.email) {
     const { requestId } = await courier.send(
       {
@@ -50,7 +60,7 @@ const handler: Handler = async (
           },
           template: "WJKFSV1362MGZEHW9G7EMMPZDMMW",
           data: {
-            amount: amount.toFixed(9),
+            amount: amount.toFixed(9).replace(/0+$/, ""),
             payer: body.payer,
           },
         },
@@ -63,6 +73,7 @@ const handler: Handler = async (
     console.log(`Email sent with requestId: ${requestId}`);
   }
 
+  // If a phone number is provided, send an SMS
   if (body.phone) {
     const { requestId } = await courier.send(
       {
@@ -72,7 +83,7 @@ const handler: Handler = async (
           },
           template: "3X4D3DD3J5MBS6GPS5K2ZHESK3HW",
           data: {
-            amount: amount.toFixed(9),
+            amount: amount.toFixed(9).replace(/0+$/, ""),
             payer: body.payer,
           },
         },
@@ -85,6 +96,7 @@ const handler: Handler = async (
     console.log(`SMS sent with requestId: ${requestId}`);
   }
 
+  // Return the responses
   return {
     statusCode: 200,
     body: JSON.stringify(responses),
